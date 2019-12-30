80bfa74b91e3c6478f30d0eb5bb40d8957eff43d
jonatan menachem <jonatan1040@gmail.com>
2019-12-30 12:24:46 +0200

    working memory game with cards flip and non clicable cards with dealys

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ class Game {
         this.card1;
         this.card2;
         this.firstevent;
+        this.image = $('img');
         this.cards_event();
     }
 
@@ -20,42 +21,33 @@ class Game {
     cards_event() {
         for (let i = 0; i < this.board.cols.length; i++) {
             this.board.cols[i].addEventListener('click', (e) => {
-                console.log(event.target.attributes.src.ownerElement.offsetParent);
                 this.flip_card_to_img(e);
             });
-            // console.log(this.board.cols);
         }
     }
 
     //check if 2 card flipped are a match or not
     check_flipped_cards(event) {
-        console.log(event.target.attributes.src.ownerElement.offsetParent);
         this.card_counter++; //count how many cards flipped (start from zero)
         //first card flipped
         if (this.card_counter < 2) {
             this.card1 = event.target.src.slice(event.target.src.search('s/') + 2).replace(/\d+/, ''); //take from terget src name of image (clean path till after images folder + remove numbers)
-            console.log(this.card1);
-            this.firstevent = event;
+            this.firstevent = event; //save the first event of first card flipped
             //secound time flipped
         } else {
             this.card2 = event.target.src.slice(event.target.src.search('s/') + 2).replace(/\d+/, ''); //take from terget src name of image (clean path till after images folder + remove numbers)
-            console.log(this.card2);
             //check cards do match
             if (this.card1 == this.card2) {
                 this.check_match_won();
                 //cards not match
             } else {
-                // alert('no match');
-                console.log(this.firstevent);
-                console.log(event);
-                // var that = this;
-                // setTimeout(function() {
-                //     that.flip_img_to_card();
-                // }, 4000);
-                // setTimeout(this.flip_img_to_card, 10000, event);
-                // setTimeout(this.flip_img_to_card, 10000, this.firstevent);
-                this.flip_img_to_card(event);
-                this.flip_img_to_card(this.firstevent);
+                for (let i = 0; i < this.board.cols.length; i++) {
+                    this.board.cols[i].classList.add('not_clickable');
+                }
+                setTimeout(() => {
+                    this.flip_img_to_card(event);
+                    this.flip_img_to_card(this.firstevent)
+                }, 2000);
             }
             this.card_counter = 0; //reset counter how many times i flipped a card
         }
@@ -63,46 +55,43 @@ class Game {
 
     check_match_won() {
         this.match_pair++; //count how many times i had paires that match
-        // console.log(this.match_pair);
         if (this.match_pair === this.board.images_arr.length / 2) {
-            // alert('you won');
             $('.modal').modal(focus);
+            // console.log($('close'));
+            // $('close').addEventListener('click', (e) => {
+            //     this.flip_card_to_img(e);
+            // });
         }
     }
 
     flip_card_to_img(event) {
-        console.log(event.target.classList);
         event.target.classList.replace('not_flipped', 'flipped');
-        console.log(event.target.classList);
-        console.log(event.target.attributes.src.ownerElement.offsetParent);
         this.check_flipped_cards(event);
     }
 
     flip_img_to_card(event) {
-            console.log(event.target.attributes.src.ownerElement.offsetParent);
-            console.log(this.card1);
-            console.log(this.card2);
-            event.target.classList.replace('flipped', 'not_flipped');
-            // this.check_flipped_cards(event);
+        event.target.classList.replace('flipped', 'not_flipped');
+        for (let i = 0; i < this.image.length; i++) {
+            this.board.cols[i].classList.remove('not_clickable');
         }
-        // need to add fliped class if its a match so when clicking again on thos how ar flipped will not count in match count
+    }
 }
 
 class Board {
     constructor() {
         //images for cards
-        this.image1 = './images/cat1.jpg';
-        this.image2 = './images/cat2.jpg';
-        this.image3 = './images/fox3.jpg';
-        this.image4 = './images/horse4.jpg';
-        this.image5 = './images/dog5.jpg';
-        this.image6 = './images/dog6.jpg';
-        this.image7 = './images/monky7.jpg';
-        this.image8 = './images/horse8.jpg';
-        this.image9 = './images/fox9.jpg';
-        this.image10 = './images/monky10.jpg';
-        this.image11 = ' ';
-        this.image12 = ' ';
+        this.image1 = './images/bibi1.jpg';
+        this.image2 = './images/bibi2.jpg';
+        this.image3 = './images/bogi1.jpg';
+        this.image4 = './images/bogi2.jpg';
+        this.image5 = './images/ganz1.jpg';
+        this.image6 = './images/ganz2.jpg';
+        this.image7 = './images/liberman1.jpg';
+        this.image8 = './images/liberman2.jpg';
+        this.image9 = './images/miri1.jpg';
+        this.image10 = './images/miri2.jpg';
+        this.image11 = './images/yair1.jpg';
+        this.image12 = './images/yair2.jpg';
         this.image13 = ' ';
         this.image14 = ' ';
         this.image15 = ' ';
@@ -117,7 +106,6 @@ class Board {
         this.image24 = ' ';
         this.images_arr = [];
         this.cols = $('.col-3');
-        // console.log(this.cols);
     }
 
     //clear board
@@ -134,7 +122,6 @@ class Board {
         for (let i = 1; i < 25; i++) {
             let image = eval('this.image' + i);
             this.images_arr.unshift(image);
-            // console.log(this.images_arr);
             if (this.images_arr[0].startsWith('./images/') === false) {
                 this.images_arr.splice(0, 1);
             }
@@ -163,6 +150,7 @@ class Board {
             let col = this.cols[i];
             col.append(img);
             img.classList.add('not_flipped');
+            // img.classList.add('clickable');
         }
     }
 }
